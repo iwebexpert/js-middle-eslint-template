9a6ff5baa8097e2f0d057f10dd7c39aa1b89d6a2
Igor Filimonov <expert@i-filimonov.com>
2024-12-28 13:00:14 +0300

    Add TypeScript support to ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,20 @@ import globals from 'globals'
 import prettier from 'eslint-plugin-prettier'
 import prettierConfig from 'eslint-config-prettier'
 import eslintPluginUnicorn from 'eslint-plugin-unicorn'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tsEslint from 'typescript-eslint'
 
-/** @type { import("eslint").Linter.Config[] } */
-export default [
-  js.configs.recommended,
-  eslintPluginUnicorn.configs['flat/recommended'],
-  prettierConfig,
-  { ignores: ['dist', 'help', 'webpack.config.js'] },
+export default tsEslint.config([
+  { ignores: ['dist', 'help', '**/*.cjs'] },
   {
-    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      eslintPluginUnicorn.configs['flat/recommended'],
+      ...tsEslint.configs.recommended,
+      prettierConfig,
+    ],
+    files: ['**/*.{js,jsx,ts,tsx,mjs}'],
     languageOptions: {
       ecmaVersion: 'latest',
       // sourceType: 'module',
@@ -19,9 +24,13 @@ export default [
       // globals: { ...globals.browser, ...globals.node },
     },
     plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
       prettier: prettier,
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': 'warn',
       'prettier/prettier': 'error',
       'arrow-body-style': 'off',
       'prefer-arrow-callback': 'off',
@@ -38,4 +47,4 @@ export default [
       'unicorn/filename-case': 'off',
     },
   },
-]
+])
